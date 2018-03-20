a48b1e2a2c43c33501cbaa7e404dcdf246a783f3
jack <997823131@qq.com>
2018-03-20 09:46:32 +0800

    yunpian

---
+++ bbs/templates/default/home/file.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by 大师兄
+ * 派系: 神秘剑派
+ * 技能: zxc秒杀
+ * Date: 2018/3/9
+ * Time: 16:44
+ * QQ:  997823131 
+ */
+?>
+<h2>文件测试</h2>
+
+<form action="<?php echo BBS_SITE_URL.DS.'index.php?act=file&op=index';?>" enctype="multipart/form-data" method="post">
+    <input name="file" type="file">
+    <input type="hidden" name="sub" value="ok"/>
+    <input type="submit" value="提交"/>
+</form>
